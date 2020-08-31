a522bdf4d30e05ddf18584c26970e666af2121a8
UnHold (Alex) <unconfigured@null.spigotmc.org>
2020-08-31 17:48:27 +0200

    added some serial communication funcions to send error- warning- and infomessages

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,54 @@
 #include <Arduino.h>
 
+//----------------Serial Communication Functions-------------------
+
+//this functions will send an information message
+void sendInformation(String info) {
+  Serial.println("I:" + info);
+}
+
+
+//this functions will send a warning message
+void sendWarning(String text) {
+  Serial.println("W:" + text);
+}
+
+
+//this function will send an error message with a given category and id
+//for the categories and ids please look at the doc
+void  sendError(uint8_t category, uint8_t id, String text) {
+  Serial.println("E:" + String(category, DEC) + ":" + String(id, DEC) + ":" + text);
+}
+
+
+void sendRespone(uint8_t spi_id, uint8_t slave_id, uint8_t bitcount, String data){
+  Serial.println("R:" + String(spi_id, DEC) + ":" + String(slave_id, DEC) + ":" + String(bitcount, DEC) + ":" + data);
+}
+
+
+
+//--------------Serial Communication Parser-----------------------
+
+//will try to get the command from the data string if any is present.
+void getCommand(String data){
+
+}
+
+
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  //init the serial communication
+  Serial.begin(115200);
+
+  //wait until the serial communication is ready (tty connected)
+  while (!Serial)
+  {
+    delay(1000);
+  }
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  
 }

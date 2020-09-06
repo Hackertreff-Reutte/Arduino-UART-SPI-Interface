fd04894b477267cfe9f92bacf5af5e92d83b634d
UnHold (Alex) <unconfigured@null.spigotmc.org>
2020-09-06 19:58:53 +0200

    fixed errors caused on linux distros

--- microcontroller/convertToInoFile.sh
+++ microcontroller/convertToInoFile.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #This script will create a folder suited for the arudino IDE.
-#Just run this script and then open the folder inside the Arduino_IDE folder called "uart-spi-converter" with the Arduino IDE
+#Just run this script and then open the folder inside the Arduino_IDE folder called "uart_spi_converter" with the Arduino IDE
 
 rm -rf ./Arduino_IDE/
 mkdir Arduino_IDE
-mkdir ./Arduino_IDE/uart-spi-converter
-touch ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "//This is a guard so that the code works with the Arduino IDE !!!Do not remove!!!" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "#define ARDUINO_IDE" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
+mkdir ./Arduino_IDE/uart_spi_converter
+touch ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "//This is a guard so that the code works with the Arduino IDE !!!Do not remove!!!" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "#define ARDUINO_IDE" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
 
 FILES=./src/*
 for file in $FILES
@@ -20,26 +20,26 @@ do
 	then
 		MAIN=$file;
 	else
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "//---------Start of the contents from the file $file------------" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		cat $file &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "//---------End of the contents from the file $file------------" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-		echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "//---------Start of the contents from the file $file------------" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		cat $file &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "//---------End of the contents from the file $file------------" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+		echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
 	fi
   fi
   
 done
 
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "//---------Start of the contents from the file $MAIN------------" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-cat $MAIN &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "//---------End of the contents from the file $MAIN------------" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
-echo "" &>> ./Arduino_IDE/uart-spi-converter/uart-spi-converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "//---------Start of the contents from the file $MAIN------------" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+cat $MAIN &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "//---------End of the contents from the file $MAIN------------" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino
+echo "" &>> ./Arduino_IDE/uart_spi_converter/uart_spi_converter.ino

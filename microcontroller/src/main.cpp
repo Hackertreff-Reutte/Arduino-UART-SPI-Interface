be2511673ccb4f0c05909aa87663d45c34d6aaf2
tabaluga34 <tabaluga.3400@gmail.com>
2020-09-09 10:58:51 +0200

    Added trimWhitespace and String split function getCommandInfo

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-
 #ifndef ARDUINO_IDE
 #include "SPIc.h"
 #endif
@@ -30,19 +29,62 @@ void sendRespone(uint8_t spi_id, uint8_t slave_id, uint8_t bitcount, String data
   Serial.println("R:" + String(spi_id, DEC) + ":" + String(slave_id, DEC) + ":" + String(bitcount, DEC) + ":" + data);
 }
 
-
-
-//---------------------------SPI--------------------------------
-
-
 //--------------Serial Communication Parser-----------------------
+// this function defines what is considered a whitespace
+bool isSpace(char c) {
+ 	return (c == ' ' || c == '\n' || c == '\r' ||
+		   c == '\t' || c == '\v' || c == '\f');
+}
 
-//will try to get the command from the data string if any is present.
-void getCommand(String data){
-  
+// this function removes all whitespaces in front of another charecter
+String trimWhitespaces(String data){
+   String temp = data;
+ 
+   if(!temp.length() == 0){
+     for (int i = 0; i < temp.length(); i++)
+     {
+       if(!isSpace(temp.charAt(0))){
+         return "";
+       }
+       else{
+         temp.remove(0);
+       }
+     }
+   }
+   return temp;
+}
+
+// this function splits the data by the given separator and returns it using a String array
+String* getCommandInfo(String data, char separator)
+{
+    String info[10]; // changes here to max number of seperated fields that are allowed
+
+    int section = 0;
+    int lastSeperator = 0;
+    for (int i = 0; i <= data.length(); i++) {
+        if (data.charAt(i) == separator) {
+            info[section] = data.substring(lastSeperator + 1, i);
+            lastSeperator = i;
+        }
+    }
+    return info;
 }
 
 
+//will try to get the command from the data string if any is present
+void getCommand(String data){
+
+  // remove whitespaces infront of other characters 
+  String preprocessed = trimWhitespaces(data);
+  
+  // check if first character is a C otherwise data is no command
+  if(preprocessed.charAt(0) == 'C'){  
+      }
+  else{
+    // no valid command
+    //sendError("parser", idfornovalidcommand, "Data is not a valid command" )
+  }
+}
 
 void setup() {
 

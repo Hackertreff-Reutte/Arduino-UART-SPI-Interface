61df5ad3734bb4e4cb5143e846ea97705a1aeb6d
tabaluga34 <tabaluga.3400@gmail.com>
2020-09-11 15:43:17 +0200

    Added error codes for command method, added parse functionality for parse function (needs to be improved)

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -6,7 +6,6 @@
 #endif
 
 
-
 //----------------Serial Communication Functions-------------------
 
 //this functions will send an information message
@@ -86,13 +85,104 @@ void getCommand(String data){
   // remove whitespaces infront of other characters 
   String preprocessed = trimWhitespaces(data);
   
-  // check if first character is a C otherwise data is no command
-  if(preprocessed.charAt(0) == 'C'){  
+  if(preprocessed.length() != 0){
+    // check if first character is a C otherwise data is no command
+    if(preprocessed.charAt(0) == 'C'){ 
+      // split command
+      String * commandinfo = getCommandInfo(preprocessed, ':');
+
+      // parse according to command type
+      if(commandinfo[1] == "TX"){
+        if(commandinfo[5] != NULL){
+          uint32_t spi_id = commandinfo[2].toInt();
+          uint32_t slave_id = commandinfo[3].toInt();
+          uint32_t bitcount = commandinfo[4].toInt();
+          uint32_t txdata = commandinfo[5].toInt();
+
+          if(txdata == bitcount){
+            transfer(spi_id, slave_id, bitcount, txdata);
+          }
+          else{
+            // sendError("parser", "P3", "Bitcount does not match data length" );
+          }
+        }
+        else{
+          // sendError("parser", "P2", "Data does not contain enough fields for valid TX command");
+        }
       }
-  else{
-    // no valid command
-    //sendError("parser", idfornovalidcommand, "Data is not a valid command" )
+      else if("SE"){
+        if(commandinfo[2] == "SPI"){
+          if(commandinfo[9] != NULL){
+            uint32_t spi_id = commandinfo[3].toInt();
+            uint32_t miso = commandinfo[4].toInt();
+            uint32_t mosi = commandinfo[5].toInt();
+            uint32_t clock = commandinfo[6].toInt();
+            uint32_t speed = commandinfo[7].toInt();
+            uint32_t bitorder = commandinfo[8].toInt();
+            uint32_t mode = commandinfo[9].toInt();
+            setupSPI(spi_id, miso, mosi, clock, speed, bitorder, mode);
+          }
+          else{
+            // sendError("parser", "P2, "Data does not contain enough fields for valid SE:SPI command");
+          }
+        }
+        else if(commandinfo[2] == "MODE"){
+          if(commandinfo[4] != NULL){
+            uint32_t spi_id = commandinfo[3].toInt();
+            uint32_t mode = commandinfo[4].toInt();
+            updateSpiMode(spi_id, mode);
+          }
+          else{
+            // sendError("parser", "P2", "Data does not contain enough fields for valid SE:MODE command");
+          }
+        }
+        else if(commandinfo[2] == "SPEED"){
+          if(commandinfo[4] != NULL){
+            uint32_t spi_id = commandinfo[3].toInt();
+            uint32_t speed = commandinfo[4].toInt();
+            updateSpiSpeed(spi_id, speed);
+          }
+          else{
+            // sendError("parser", "P2", "Data does not contain enough fields for valid SE:SPEED command");
+          }
+        }
+        else if(commandinfo[2] == "SLAVE"){
+          if(commandinfo[5] != NULL){
+            uint32_t spi_id = commandinfo[3].toInt();
+            uint32_t slave_id = commandinfo[4].toInt();
+            uint32_t pin = commandinfo[5].toInt();
+            setupSlave(spi_id, slave_id, pin);
+          }
+          else{
+            // sendError("parser", "P2", "Data does not contain enough fields for valid SE:SLAVE command");
+          }
+        }
+        else{
+        // sendError("parser", "P4", "Invalid setup command");
+        }
+      }
+      else if("ST"){
+      Serial.write("Call st function");
+      }
+      else if("BS"){
+      Serial.write("Call bs function");
+      }
+      else if("BE"){
+      Serial.write("Call be function");
+      }
+      else if("BT"){
+      Serial.write("Call bt function");
+      }
+    }
+    else{
+      // no valid command
+      //sendError("parser", "P1", "Data is not a valid command");
+    }
   }
+  else{
+    //sendError("parser", "P0", "Empty data");
+  }
+  
 }
 
 void setup() {

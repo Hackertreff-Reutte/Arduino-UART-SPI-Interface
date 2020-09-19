933c470750e5dabb414d7371088788a733f1d32f
UnHold (Alex) <unconfigured@null.spigotmc.org>
2020-09-19 23:45:53 +0200

    added unit test for the spi id bound check, spi slave id bound check and the valid bitcount check

--- microcontroller/src/settings.h
+++ microcontroller/src/settings.h
@@ -1,7 +1,13 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+//------SPI------
+#define SIZE_OF_SPICONTROLLER_ARRAY 4  //number of possible SPIs
+#define NUMBER_OF_SLAVES_PER_SPI 8
+
+//#define DEBUG
 
 #define UART_SPEED 115200
+
 
 #endif

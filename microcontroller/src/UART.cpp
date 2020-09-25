aa309b01b4366b504914dba59bd38014c73c88f5
UnHold (Alex) <unconfigured@null.spigotmc.org>
2020-09-25 13:13:50 +0200

    added GPL license + header

--- microcontroller/src/UART.cpp
+++ microcontroller/src/UART.cpp
@@ -1,3 +1,23 @@
+/*
+        UART-SPI Interface to easily debug or script SPI devices with a PC / UART-Interface. 
+        Copyright (C) 2020  Alex Hold (UnHold)
+        Copyright (C) 2020  Alina Schärmer (tabaluga34 / ina06)
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 
 //this ifndef is just here for a script to create a single main file (used by the arduino ide) and to not include non-existend files 
 #ifndef ARDUINO_IDE
--- microcontroller/src/UART.h
+++ microcontroller/src/UART.h
@@ -1,3 +1,24 @@
+/*
+        UART-SPI Interface to easily debug or script SPI devices with a PC / UART-Interface. 
+        Copyright (C) 2020  Alex Hold (UnHold)
+        Copyright (C) 2020  Alina Schärmer (tabaluga34 / ina06)
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
 #ifndef UART_H
 #define UART_H
 
